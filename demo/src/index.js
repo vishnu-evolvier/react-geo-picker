6f8164ab3a275596347181458781977c6aee5082
stupid <sunfeng32@qq.com>
2018-03-16 17:27:39 +0800

    feat(demo): just display

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,23 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
 
-import Example from '../../src'
+import MapboxGeoPicker from '../../src/mapbox';
+import GoogleMapGeoPicker from '../../src/google-map';
 
 class Demo extends Component {
+  state = {
+    location,
+  };
+
+  onLocationChange = (location) => this.setState({ location });
+
   render() {
     return <div>
       <h1>react-geo-picker Demo</h1>
-      <Example/>
+      <MapboxGeoPicker value={this.state.location} onChange={this.onLocationChange} />
+      <GoogleMapGeoPicker value={this.state.location} onChange={this.onLocationChange} />
     </div>
   }
 }
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo/>, document.querySelector('#demo'));
