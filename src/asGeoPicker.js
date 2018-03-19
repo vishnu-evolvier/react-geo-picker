7f8ad22e43faa648e62e82bd872a8c651f9dc60b
stupid <sunfeng32@qq.com>
2018-03-19 18:18:28 +0800

    feat(asGeoPicker): provide props for any GeoPicker

---
+++ src/asGeoPicker.js
@@ -0,0 +1,78 @@
+import compose from 'recompose/compose';
+import withState from 'recompose/withState';
+import mapProps from 'recompose/mapProps';
+import withDefaultProps from 'recompose/defaultProps';
+import withHandlers from 'recompose/withHandlers';
+
+export const defaultProps = {
+  value: undefined,
+  onChange: undefined,
+  defaultValue: undefined,
+  propNameMap: {
+    location: 'location',
+    latitude: 'latitude',
+    longitude: 'longitude',
+    onLocationChange: 'onLocationChange',
+  },
+};
+
+export const propsMapper = ({ value, location, ...rest }) => ({
+  width: 400,
+  height: 300,
+  location: value || location,
+  ...rest,
+});
+
+export const initialState = ({
+  defaultValue: { latitude, longitude } = {},
+}) => ({
+  latitude: latitude || 0,
+  longitude: longitude || 0,
+});
+
+export const onLocationChange = ({
+  propNameMap = {},
+  value,
+  onChange,
+  setLocation,
+}) => ({
+  [propNameMap.latitude]: latitude,
+  [propNameMap.longitude]: longitude,
+} = {}) => {
+  const location = { latitude, longitude };
+  if (onChange) {
+    onChange(location);
+  }
+
+  if (!value) {
+    setLocation(location);
+  }
+};
+
+export const propNameMapper = ({
+  propNameMap = {},
+  location,
+  onLocationChange,
+  ...rest,
+}) => ({
+  ...rest,
+  [propNameMap.location]: location ? {
+    [propNameMap.latitude]: location.latitude,
+    [propNameMap.longitude]: location.longitude,
+  } : location,
+  [propNameMap.onLocationChange]: onLocationChange,
+});
+
+export default ({ propNameMap } = {}) => compose(
+  withDefaultProps({
+    ...defaultProps,
+    propNameMap: {
+      ...defaultProps.propNameMap,
+      ...propNameMap,
+    },
+  }),
+  withState('location', 'setLocation'),
+  mapProps(propsMapper),
+  withHandlers({ onLocationChange }),
+  mapProps(propNameMapper),
+);
