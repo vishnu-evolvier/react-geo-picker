2f56fba57c1ed89a5a17d81b5187a62597f3bd12
stupid <sunfeng32@qq.com>
2018-03-20 16:17:16 +0800

    feat(GoogleMapGeoPicker): add searchBox

--- src/google-map/index.js
+++ src/google-map/index.js
@@ -1,28 +1,112 @@
 import React from "react";
-import { compose, withProps } from "recompose";
-import {
+import SearchBox from 'react-google-maps/lib/components/places/SearchBox';
+
+import compose from 'recompose/compose';
+import mapProps from 'recompose/mapProps';
+
+import withScriptjs from 'react-google-maps/lib/withScriptjs';
+import withGoogleMap from 'react-google-maps/lib/withGoogleMap';
+import GoogleMap from 'react-google-maps/lib/components/GoogleMap';
+import Marker from 'react-google-maps/lib/components/Marker';
+
+import asGeoPicker from '../asGeoPicker';
+import './index.css';
+
+class GoogleMapGeoPicker extends React.Component {
+  state = {
+    bounds: null,
+  };
+
+  handlePlacesChanged = () => {
+    const places = this.searchBox.getPlaces();
+    if (!places || !places.length) return;
+    const place = places[0];
+
+    this.setCenter(place.geometry.location);
+  };
+
+  handleCenterChanged = () => this.setCenter(this.map.getCenter());
+
+  setCenter = (center) => {
+    const { onCenterChange } = this.props;
+    if (!onCenterChange) return ;
+    const { lat, lng } = center;
+    onCenterChange({
+      lat: lat(),
+      lng: lng(),
+    });
+  };
+
+  render() {
+    const {
+      defaultCenter,
+      center = defaultCenter,
+    } = this.props;
+
+    return (
+      <GoogleMap
+        defaultZoom={8}
+        defaultCenter={defaultCenter}
+        defaultOptions={{
+          mapTypeControl: false,
+          scaleControl: false,
+          streetViewControl: false,
+        }}
+        center={center}
+        ref={c => this.map = c}
+        onCenterChanged={this.handleCenterChanged}
+      >
+        <Marker position={center} />
+        <SearchBox
+          ref={c => this.searchBox = c}
+          bounds={this.state.bounds}
+          controlPosition={google.maps.ControlPosition.TOP_LEFT}
+          onPlacesChanged={this.handlePlacesChanged}
+        >
+          <input
+            className="searchBox"
+            type="text"
+            placeholder="Type to Search"
+          />
+        </SearchBox>
+      </GoogleMap>
+    )
+  }
+}
+
+const defaultCenter = {
+  lat: -34.397,
+  lng: 150.644,
+};
+
+GoogleMapGeoPicker.defaultProps = {
+  defaultCenter,
+};
+
+const propsMapper = ({
+  width,
+  height,
+  apiKey = '',
+  ...rest,
+}) => ({
+  ...rest,
+  googleMapURL:
+    `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=geometry,drawing,places`,
+  loadingElement: <div style={{ height: `100%` }} />,
+  containerElement: <div style={{ height, width }} />,
+  mapElement: <div style={{ height: `100%` }} />,
+});
+
+const propNameMap = {
+  location: 'center',
+  onLocationChange: 'onCenterChange',
+  latitude: 'lat',
+  longitude: 'lng',
+};
+
+export default compose(
+  asGeoPicker({ propNameMap }),
+  mapProps(propsMapper),
   withScriptjs,
   withGoogleMap,
-  GoogleMap,
-  Marker
-} from "react-google-maps";
-
-const apiKey = '';
-
-const GoogleMapGeoPicker = compose(
-  withProps({
-    googleMapURL:
-      `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=geometry,drawing,places`,
-    loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `400px` }} />,
-    mapElement: <div style={{ height: `100%` }} />
-  }),
-  withScriptjs,
-  withGoogleMap
-)(props => (
-  <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
-    <Marker position={{ lat: -34.397, lng: 150.644 }} />
-  </GoogleMap>
-));
-
-export default GoogleMapGeoPicker;
+)(GoogleMapGeoPicker);
