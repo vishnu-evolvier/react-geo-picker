28af0aea70b8d0abee45c3f7bf94d7304bc7338c
stupid <sunfeng32@qq.com>
2018-03-20 16:20:17 +0800

    feat(demo): add inputs demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -4,19 +4,65 @@ import {render} from 'react-dom'
 import MapboxGeoPicker from '../../src/mapbox';
 import GoogleMapGeoPicker from '../../src/google-map';
 
+import './index.css';
+
 class Demo extends Component {
   state = {
-    location: undefined,
+    location: {
+      latitude: 31.1094,
+      longitude: 121.4114,
+    },
   };
 
-  onLocationChange = (location) => this.setState({ location });
+  onLocationChange = (location) => this.setState({
+    location: {
+      ...this.state.location,
+      ...location,
+    }
+  });
+
+  handleLatitudeChange = (event) => this.onLocationChange({
+    latitude: Number(event.target.value),
+  });
+
+  handleLongitudeChange = (event) => this.onLocationChange({
+    longitude: Number(event.target.value),
+  });
 
   render() {
-    return <div>
-      <h1>react-geo-picker Demo</h1>
-      <MapboxGeoPicker value={this.state.location} onChange={this.onLocationChange} />
-      <GoogleMapGeoPicker value={this.state.location} onChange={this.onLocationChange} />
-    </div>
+    const { location } = this.state;
+
+    return (
+      <div className="container">
+        <h1>react-geo-picker Demo</h1>
+        <div className="input-control">
+          <label>Latitude:</label>
+          <input
+            className="input"
+            type="number"
+            step="0.0000001"
+            min={-90}
+            max={90}
+            value={location.latitude}
+            onChange={this.handleLatitudeChange}
+          />
+        </div>
+        <div className="input-control">
+          <label>Latitude:</label>
+          <input
+            className="input"
+            type="number"
+            step={0.0000001}
+            min={-180}
+            max={180}
+            value={location.longitude}
+            onChange={this.handleLongitudeChange}
+          />
+        </div>
+        <MapboxGeoPicker value={location} onChange={this.onLocationChange} />
+        <GoogleMapGeoPicker value={location} onChange={this.onLocationChange} />
+      </div>
+    )
   }
 }
 
